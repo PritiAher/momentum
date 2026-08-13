97d914161fd1e26ed99f3c92eb3aaa06814f6f05
PritiAher <pritiaher301@gmail.com>
2026-08-13 23:22:56 +0530

    fix cors error

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -16,7 +16,7 @@ const generateTokenAndSetCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: days * 24 * 60 * 60 * 1000,
   });
 
